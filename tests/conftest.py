a431ffc4911ee461420fa0216d421a52f79dd5f3
PrinceWebCoder <bekjanov2310@gmail.com>
2025-06-11 17:36:58 +0500

    Type hints added to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,12 @@
+from __future__ import annotations
+
+import collections.abc as cabc
+
 import pytest
 from flask import Flask as _Flask
+from flask.ctx import AppContext
+from flask.ctx import RequestContext
+from flask.testing import FlaskClient
 
 
 class Flask(_Flask):
@@ -14,23 +21,23 @@ class Flask(_Flask):
 
 
 @pytest.fixture
-def app():
+def app() -> Flask:
     app = Flask(__name__)
     return app
 
 
 @pytest.fixture
-def app_ctx(app):
+def app_ctx(app: Flask) -> cabc.Generator[AppContext, None, None]:
     with app.app_context() as ctx:
         yield ctx
 
 
 @pytest.fixture
-def req_ctx(app):
+def req_ctx(app: Flask) -> cabc.Generator[RequestContext, None, None]:
     with app.test_request_context() as ctx:
         yield ctx
 
 
 @pytest.fixture
-def client(app):
+def client(app: Flask) -> FlaskClient:
     return app.test_client()

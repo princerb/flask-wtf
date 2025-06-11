c3c52e41c4aea617f68a379365a7caf8d0afdc99
PrinceWebCoder <bekjanov2310@gmail.com>
2025-06-11 17:37:16 +0500

    Type hints added to test_csrf_extension.py

--- tests/test_csrf_extension.py
+++ tests/test_csrf_extension.py
@@ -1,7 +1,14 @@
+from __future__ import annotations
+
 import pytest
 from flask import Blueprint
+from flask import Flask
 from flask import g
 from flask import render_template_string
+from flask.ctx import AppContext
+from flask.ctx import RequestContext
+from flask.testing import FlaskClient
+from flask.wrappers import Response
 
 from flask_wtf import FlaskForm
 from flask_wtf.csrf import CSRFError
@@ -10,7 +17,7 @@ from flask_wtf.csrf import generate_csrf
 
 
 @pytest.fixture
-def app(app):
+def app(app: Flask) -> Flask:
     CSRFProtect(app)
 
     @app.route("/", methods=["GET", "POST"])
@@ -18,7 +25,7 @@ def app(app):
         pass
 
     @app.after_request
-    def add_csrf_header(response):
+    def add_csrf_header(response: Response) -> Response:
         response.headers.set("X-CSRF-Token", generate_csrf())
         return response
 
@@ -26,16 +33,16 @@ def app(app):
 
 
 @pytest.fixture
-def csrf(app):
+def csrf(app: Flask) -> CSRFProtect:
     return app.extensions["csrf"]
 
 
-def test_render_token(req_ctx):
+def test_render_token(req_ctx: RequestContext) -> None:
     token = generate_csrf()
     assert render_template_string("{{ csrf_token() }}") == token
 
 
-def test_protect(app, client, app_ctx):
+def test_protect(app: Flask, client: FlaskClient, app_ctx: AppContext) -> None:
     response = client.post("/")
     assert response.status_code == 400
     assert "The CSRF token is missing." in response.get_data(as_text=True)
@@ -60,7 +67,7 @@ def test_protect(app, client, app_ctx):
     assert client.post("/", headers={"X-CSRF-Token": token}).status_code == 200
 
 
-def test_same_origin(client):
+def test_same_origin(client: FlaskClient) -> None:
     token = client.get("/").headers["X-CSRF-Token"]
     response = client.post(
         "/", base_url="https://localhost", headers={"X-CSRF-Token": token}
@@ -100,13 +107,13 @@ def test_same_origin(client):
     assert response.status_code == 200
 
 
-def test_form_csrf_short_circuit(app, client):
+def test_form_csrf_short_circuit(app: Flask, client: FlaskClient) -> None:
     @app.route("/skip", methods=["POST"])
     def skip():
         assert g.get("csrf_valid")
         # don't pass the token, then validate the form
         # this would fail if CSRFProtect didn't run
-        form = FlaskForm(None)
+        form: FlaskForm = FlaskForm(None)
         assert form.validate()
 
     token = client.get("/").headers["X-CSRF-Token"]
@@ -114,7 +121,7 @@ def test_form_csrf_short_circuit(app, client):
     assert response.status_code == 200
 
 
-def test_exempt_view(app, csrf, client):
+def test_exempt_view(app: Flask, csrf: CSRFProtect, client: FlaskClient) -> None:
     @app.route("/exempt", methods=["POST"])
     @csrf.exempt
     def exempt():
@@ -128,7 +135,7 @@ def test_exempt_view(app, csrf, client):
     assert response.status_code == 200
 
 
-def test_manual_protect(app, csrf, client):
+def test_manual_protect(app: Flask, csrf: CSRFProtect, client: FlaskClient) -> None:
     @app.route("/manual", methods=["GET", "POST"])
     @csrf.exempt
     def manual():
@@ -141,7 +148,7 @@ def test_manual_protect(app, csrf, client):
     assert response.status_code == 400
 
 
-def test_exempt_blueprint(app, csrf, client):
+def test_exempt_blueprint(app: Flask, csrf: CSRFProtect, client: FlaskClient) -> None:
     bp = Blueprint("exempt", __name__, url_prefix="/exempt")
     csrf.exempt(bp)
 
@@ -154,7 +161,9 @@ def test_exempt_blueprint(app, csrf, client):
     assert response.status_code == 200
 
 
-def test_exempt_nested_blueprint(app, csrf, client):
+def test_exempt_nested_blueprint(
+    app: Flask, csrf: CSRFProtect, client: FlaskClient
+) -> None:
     bp1 = Blueprint("exempt1", __name__, url_prefix="/")
     bp2 = Blueprint("exempt2", __name__, url_prefix="/exempt")
     csrf.exempt(bp2)
@@ -170,21 +179,23 @@ def test_exempt_nested_blueprint(app, csrf, client):
     assert response.status_code == 200
 
 
-def test_error_handler(app, client):
+def test_error_handler(app: Flask, client: FlaskClient) -> None:
     @app.errorhandler(CSRFError)
-    def handle_csrf_error(e):
+    def handle_csrf_error(e: CSRFError) -> str:
         return e.description.lower()
 
     response = client.post("/")
     assert response.get_data(as_text=True) == "the csrf token is missing."
 
 
-def test_validate_error_logged(client, monkeypatch):
+def test_validate_error_logged(
+    client: FlaskClient, monkeypatch: pytest.MonkeyPatch
+) -> None:
     from flask_wtf.csrf import logger
 
-    messages = []
+    messages: list[str] = []
 
-    def assert_info(message):
+    def assert_info(message: str) -> None:
         messages.append(message)
 
     monkeypatch.setattr(logger, "info", assert_info)
